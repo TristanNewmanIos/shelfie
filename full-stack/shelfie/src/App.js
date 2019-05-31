a2fdf944675c251185d45b2830f690daa42948eb
tristan <tristanb.newman@gmail.com>
2019-05-31 10:25:26 -0500

    Intial commit. Component layout

---
+++ full-stack/shelfie/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './Components/Header/Header'
+import Dashboard from './Components/Dashboard/Dashboard'
+import Form from './Components/Form/Form'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Dashboard/>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
